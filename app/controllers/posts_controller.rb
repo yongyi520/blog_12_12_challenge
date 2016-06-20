a95543bf7287d6f656f0a640fea4a5c6e295db67
yongyi <reach.yongyixu@gmail.com>
2016-06-20 15:57:20 +0700

    added edit and delete post functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,16 +16,31 @@ class PostsController < ApplicationController
     else
       render 'new'
     end
-
-
   end
 
   def show
     @post = Post.find(params[:id])
   end
 
-  def destroy
+  def edit
+    @post = Post.find(params[:id])
+  end
 
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(params[:post].permit(:title, :body))
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to root_path
   end
 
   private
