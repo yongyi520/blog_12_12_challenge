0067b5ca86e6a4a7edd5523c3b4771b71e367fc4
yongyi <reach.yongyixu@gmail.com>
2016-06-20 16:21:55 +0700

    comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :posts
+  resources :posts do
+    resources :comments
+  end
   root "posts#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
